37e929400bc95480c3861b21a20a9db580c68c22
Abdelghani Moussaid <20841568+abdelghani-moussaid@users.noreply.github.com>
2024-05-06 12:25:09 +0100

    Remove instances from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    for(let i = 0; i < elements.length; i++) {
+        let index = array.indexOf(elements[i]);
+        while(index != -1){
+            array.splice(index, 1);
+            index = array.indexOf(elements[i]);
+        }
+    }
+    return array;
+    
 };
 
 // Do not edit below this line
